b2b69ab3241fd6b8984b0bd2710885df14c47afe
Ricardo Peres <rjperes@hotmail.com>
2024-11-25 10:36:14 +0000

    Adding history property to attribute

--- EFAuditable/AuditableAttribute.cs
+++ EFAuditable/AuditableAttribute.cs
@@ -4,5 +4,6 @@
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public sealed class AuditableAttribute : Attribute
     {
+        public bool History { get; init; }
     }
 }
